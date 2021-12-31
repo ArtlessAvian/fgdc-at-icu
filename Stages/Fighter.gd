df9ead0f173741ec7ec561fc222e3dd85b3b9d2c
ArtlessAvian <artlessavian@gmail.com>
2021-12-30 20:04:25 -0800

    initial commit

---
+++ Stages/Fighter.gd
@@ -0,0 +1,38 @@
+extends Sprite3D
+
+const x_bound = 10; # |x| can't go > x_bound
+const y_bound = 0; # y can't go < 0
+
+var vel : Vector2 = Vector2()
+var grounded : bool = true
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	get_input()
+	move(delta)
+
+func get_input():
+	if grounded:
+		vel.x = 0;
+		if Input.is_action_pressed("ui_left"):
+			vel.x -= 5
+		if Input.is_action_pressed("ui_right"):
+			vel.x += 5
+		if Input.is_action_pressed("ui_up"):
+			grounded = false
+			vel.y += 10
+
+func move(delta):
+	if not grounded:
+		vel.y -= 20 * delta
+
+	self.translation += Vector3(vel.x, vel.y, 0) * delta
+
+	if self.translation.y < 0:
+		grounded = true
+		self.translation.y = 0
+
+	if self.translation.x > x_bound - $Collision.scale.x/2:
+		self.translation.x = x_bound - $Collision.scale.x/2
---
+++ Stages/StageCamera.gd
@@ -0,0 +1,10 @@
+extends Camera
+
+export var fighter_one : NodePath = "";
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
