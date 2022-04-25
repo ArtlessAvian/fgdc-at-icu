ed330b4f1793820571b531a476a03e1e3f6b75a1
Ryan <artlessavian@gmail.com>
2022-04-25 04:31:12 -0700

    Jump Cancel on Contact

--- addons/FGDC-Datatypes/GenericStates/NormalAttackState.gd
+++ addons/FGDC-Datatypes/GenericStates/NormalAttackState.gd
@@ -3,6 +3,7 @@ extends "../State.gd"
 export(Resource) var attack_data = null
 
 export(int) var attack_level = 0
+export(bool) var jump_cancellable = false
 
 
 func ensure_not_null():
@@ -23,6 +24,17 @@ func transition_out(f: Fighter, moveset: Moveset, input: Dictionary) -> State:
 	var attack = transition_into_attack(f, moveset, input)
 	if attack != null:
 		return attack
+
+	# TODO: Rethink Hitbox, Hurtbox interation.
+	# This checks for contact but /not/ hitting.
+	if f.get_node("Hitboxes").attack_number == f.state_dict.last_attack_hit:
+		var jump
+		if f.grounded:
+			jump = transition_into_jump(f, moveset, input)
+		else:
+			jump = transition_into_double_jump(f, moveset, input)
+		if jump != null:
+			return jump
 
 	if f.state_time > attack_data.animation_length():
 		if not f.grounded:
