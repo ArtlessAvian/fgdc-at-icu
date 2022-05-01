2b763a9126c528b0b7b4e50182ad34ef4e72adc8
Alexander Grams <alexgrams222@gmail.com>
2022-04-30 20:53:32 -0700

    Added prejump
    Fighter transitions into Prejump state before Jump. Code for moving the Fighter upwards, previously in State.transition_into_jump(), has been moved to PrejumpState.transition_out() and is run before Fighter transitions into the Jump state. A "prejump" property has been added to Moveset and should be set for every fighter.

---
+++ addons/FGDC-Datatypes/GenericStates/PrejumpState.gd
@@ -0,0 +1,42 @@
+extends "../State.gd"
+
+export(int) var impulse = 24 * 65536
+export(int) var double_jump_impulse = 16 * 65536
+export(int) var horizontal_speed = 10 * 65536
+
+const prejump_frames = 3 
+
+
+func transition_out(f: Fighter, moveset: Moveset, input: Dictionary) -> State:
+	var attack = transition_into_attack(f, moveset, input)
+	if attack != null:
+		return attack
+
+	if f.state_time >= prejump_frames:
+		if input.stick_x == 0:
+			f.vel.x = 0
+		elif input.stick_x < 0 and f.vel.x > -moveset.jump.horizontal_speed:
+			f.vel.x = -moveset.jump.horizontal_speed
+		elif input.stick_x > 0 and f.vel.x < moveset.jump.horizontal_speed:
+			f.vel.x = moveset.jump.horizontal_speed
+
+		f.vel.y = moveset.jump.impulse
+		f.air_actions = moveset.air_actions
+		f.grounded = false
+
+		return moveset.jump
+
+	return null
+
+
+func run(f: Fighter, input: Dictionary) -> void:
+	pass
+
+
+# TODO: Proper prejump animation
+func animation(f: Fighter) -> String:
+	return "Idle"
+
+
+func can_block() -> bool:
+	return true
