56d42b32f759fe298cc55188f6d8985748403505
Ryan <artlessavian@gmail.com>
2022-05-29 19:25:58 -0700

    Boba Running Attacks

---
+++ Characters/Boba/SeverAfterimage.gd
@@ -0,0 +1,39 @@
+extends SGFixedNode2D
+
+var flip = false
+var lifetime = 0
+
+export(Resource) var color_ramp
+
+
+func initializeee(data: Dictionary):
+	self.lifetime = 0
+	self.flip = data.flip
+
+	self.fixed_position.x = data.position_x
+	self.fixed_position.y = data.position_y
+
+	self.add_child(data.thing)
+
+	_physics_process(13431414)
+	# _network_preprocess({})
+
+
+func _physics_process(delta): #(input: Dictionary) -> void:
+	lifetime += 1
+
+	self.modulate = color_ramp.interpolate(lifetime / 30.0)
+
+	if lifetime > 20:
+		self.queue_free()
+		# SyncManager.despawn(self)
+
+
+func _save_state() -> Dictionary:
+	return {
+		lifetime = lifetime,
+	}
+
+
+func _load_state(save: Dictionary) -> void:
+	lifetime = save.lifetime
